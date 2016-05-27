a044518018d587034138eb3ad81cdee327a536f3
Robert Kimball <robert.kimball@intel.com>
2016-05-27 14:05:39 -0700

    initialize _pinned in ctor

--- loader/src/buffer.cpp
+++ loader/src/buffer.cpp
@@ -35,7 +35,7 @@ Buffer::Buffer(int size, bool pinned)
 }
 
 Buffer::Buffer(char* data, int size)
-: _data(data), _size(size), _cur(_data), _idx(0), _alloc(false) {
+: _data(data), _size(size), _cur(_data), _idx(0), _alloc(false), _pinned(false) {
 }
 
 Buffer::~Buffer() {
