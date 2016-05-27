88ccc1ed59bdcf912309811bec4a88718b67c665
Robert Kimball <robert.kimball@intel.com>
2016-05-27 14:00:19 -0700

    move Buffer implementation to cpp file

--- loader/src/buffer.cpp
+++ loader/src/buffer.cpp
@@ -28,6 +28,144 @@
 
  using namespace std;
 
+Buffer::Buffer(int size, bool pinned)
+: _size(size), _idx(0), _alloc(true), _pinned(pinned) {
+    _data = alloc();
+    _cur = _data;
+}
+
+Buffer::Buffer(char* data, int size)
+: _data(data), _size(size), _cur(_data), _idx(0), _alloc(false) {
+}
+
+Buffer::~Buffer() {
+    if (_alloc == true) {
+        dealloc(_data);
+    }
+}
+
+void Buffer::reset() {
+    _cur = _data;
+    _idx = 0;
+    _items.clear();
+    _lens.clear();
+}
+
+void Buffer::dump() {
+    uint8_t* data = reinterpret_cast<uint8_t*>(_data);
+    int len = _size;
+    assert(len % 16 == 0);
+    int index = 0;
+    while (index < len) {
+        printf("%08x", index);
+        for (int i = 0; i < 8; i++) {
+            printf(" %02x", data[i]);
+        }
+        printf("  ");
+        for (int i = 8; i < 16; i++) {
+            printf(" %02x", data[i]);
+        }
+        printf(" ");
+        for (int i = 0; i < 16; i++) {
+            printf("%c", (data[i] < 32)? '.' : data[i]);
+        }
+        printf("\n");
+        data += 16;
+        index += 16;
+    }
+}
+
+void Buffer::pushItem(int len) {
+    _items.push_back(_idx);
+    _lens.push_back(len);
+    _cur += len;
+    _idx += len;
+}
+
+char* Buffer::getItem(int index, int& len) {
+    if (index >= (int) _items.size()) {
+        return 0;
+    }
+    len = _lens[index];
+    return _data + _items[index];
+}
+
+int Buffer::getItemCount() {
+    return _items.size();
+}
+
+char* Buffer::getCurrent() {
+    return _cur;
+}
+
+uint Buffer::getSize() {
+    return _size;
+}
+
+uint Buffer::getLevel() {
+    return _idx;
+}
+
+void Buffer::read(IfStream& ifs, int size) {
+    resizeIfNeeded(size);
+    ifs.read(_cur, size);
+    pushItem(size);
+}
+
+void Buffer::read(char* src, int size) {
+    resizeIfNeeded(size);
+    memcpy((void *) _cur, (void *) src, size);
+    pushItem(size);
+}
+
+void Buffer::resizeIfNeeded(int inc) {
+    if (getLevel() + inc > getSize()) {
+        resize(inc);
+    }
+}
+
+void Buffer::resize(int inc) {
+    assert(_alloc == true);
+    _size = getLevel() + inc;
+    // Allocate a bit more to minimize reallocations.
+    _size += _size / 8;
+    char* data = alloc();
+    memcpy(data, _data, getLevel());
+    dealloc(_data);
+    _data = data;
+    _cur = _data + _idx;
+}
+
+char* Buffer::alloc() {
+    char*      data;
+    assert(_alloc == true);
+    if (_pinned == true) {
+#if HAS_GPU
+        CUresult status = cuMemAllocHost((void**)&data, _size);
+        if (status != CUDA_SUCCESS) {
+            throw std::bad_alloc();
+        }
+#else
+        data = new char[_size];
+#endif
+    } else {
+        data = new char[_size];
+    }
+    return data;
+}
+
+void Buffer::dealloc(char* data) {
+    if (_pinned == true) {
+#if HAS_GPU
+        cuMemFreeHost(data);
+#else
+        delete[] data;
+#endif
+    } else {
+        delete[] data;
+    }
+}
+
 BufferPool::BufferPool(int dataSize, int targetSize, bool pinned, int count)
 : _count(count), _used(0), _readPos(0), _writePos(0) {
     for (int i = 0; i < count; i++) {
--- loader/src/buffer.hpp
+++ loader/src/buffer.hpp
@@ -25,149 +25,32 @@
 #include <mutex>
 #include <condition_variable>
 #include <cstring>
+#include <iostream>
 
 #include "streams.hpp"
 
 class Buffer {
 public:
-    explicit Buffer(int size, bool pinned = false)
-    : _size(size), _idx(0), _alloc(true), _pinned(pinned) {
-        _data = alloc();
-        _cur = _data;
-    }
-
-    Buffer(char* data, int size)
-    : _data(data), _size(size), _cur(_data), _idx(0), _alloc(false) {
-    }
-
-    virtual ~Buffer() {
-        if (_alloc == true) {
-            dealloc(_data);
-        }
-    }
-
-    void reset() {
-        _cur = _data;
-        _idx = 0;
-        _items.clear();
-        _lens.clear();
-    }
-
-    void dump() {
-        uint8_t* data = reinterpret_cast<uint8_t*>(_data);
-        int len = _size;
-        assert(len % 16 == 0);
-        int index = 0;
-        while (index < len) {
-            printf("%08x", index);
-            for (int i = 0; i < 8; i++) {
-                printf(" %02x", data[i]);
-            }
-            printf("  ");
-            for (int i = 8; i < 16; i++) {
-                printf(" %02x", data[i]);
-            }
-            printf(" ");
-            for (int i = 0; i < 16; i++) {
-                printf("%c", (data[i] < 32)? '.' : data[i]);
-            }
-            printf("\n");
-            data += 16;
-            index += 16;
-        }
-    }
-
-    void pushItem(int len) {
-        _items.push_back(_idx);
-        _lens.push_back(len);
-        _cur += len;
-        _idx += len;
-    }
-
-    char* getItem(int index, int& len) {
-        if (index >= (int) _items.size()) {
-            return 0;
-        }
-        len = _lens[index];
-        return _data + _items[index];
-    }
-
-    int getItemCount() {
-        return _items.size();
-    }
-
-    char* getCurrent() {
-        return _cur;
-    }
-
-    uint getSize() {
-        return _size;
-    }
-
-    uint getLevel() {
-        return _idx;
-    }
-
-    void read(IfStream& ifs, int size) {
-        resizeIfNeeded(size);
-        ifs.read(_cur, size);
-        pushItem(size);
-    }
-
-    void read(char* src, int size) {
-        resizeIfNeeded(size);
-        memcpy((void *) _cur, (void *) src, size);
-        pushItem(size);
-    }
+    explicit Buffer(int size, bool pinned = false);
+    Buffer(char* data, int size);
+    virtual ~Buffer();
+    void reset();
+    void dump();
+    void pushItem(int len);
+    char* getItem(int index, int& len);
+    int getItemCount();
+    char* getCurrent();
+    uint getSize();
+    uint getLevel();
+    void read(IfStream& ifs, int size);
+    void read(char* src, int size);
 
 private:
-    void resizeIfNeeded(int inc) {
-        if (getLevel() + inc > getSize()) {
-            resize(inc);
-        }
-    }
-
-    void resize(int inc) {
-        assert(_alloc == true);
-        _size = getLevel() + inc;
-        // Allocate a bit more to minimize reallocations.
-        _size += _size / 8;
-        char* data = alloc();
-        memcpy(data, _data, getLevel());
-        dealloc(_data);
-        _data = data;
-        _cur = _data + _idx;
-    }
-
-    char* alloc() {
-        char*      data;
-        assert(_alloc == true);
-        if (_pinned == true) {
-#if HAS_GPU
-            CUresult status = cuMemAllocHost((void**)&data, _size);
-            if (status != CUDA_SUCCESS) {
-                throw std::bad_alloc();
-            }
-#else
-            data = new char[_size];
-#endif
-        } else {
-            data = new char[_size];
-        }
-        return data;
-    }
-
-    void dealloc(char* data) {
-        if (_pinned == true) {
-#if HAS_GPU
-            cuMemFreeHost(data);
-#else
-            delete[] data;
-#endif
-        } else {
-            delete[] data;
-        }
-    }
+    Buffer() = delete;
+    void resizeIfNeeded(int inc);
+    void resize(int inc);
+    char* alloc();
+    void dealloc(char* data);
 
 public:
     char*                       _data;
