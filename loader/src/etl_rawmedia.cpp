85d5aa138261ddc733c19db70857016a31c42ced
Alex Park <alex.park@gmail.com>
2016-06-13 00:18:49 -0700

    Some scaffolding for rawmedia extraction (porting over anil's codec code, mostly)

---
+++ loader/src/etl_rawmedia.cpp
@@ -0,0 +1,134 @@
+#include "etl_rawmedia.hpp"
+
+using namespace std;
+using namespace nervana;
+
+void rawmedia::decoded::copy_data(char* buf, int buf_size)
+{
+    if (_data_size * (int) _bufs.size() > buf_size) {
+        stringstream ss;
+        ss << "Buffer too small to copy decoded data. Buffer size " <<
+               buf_size << " Data size " << _data_size * _bufs.size();
+        throw runtime_error(ss.str());
+    }
+
+    for (uint i = 0; i < _bufs.size(); i++) {
+        memcpy(buf, _bufs[i], _data_size);
+        buf += _data_size;
+    }
+}
+
+void rawmedia::decoded::add_bufs(int count, int size)
+{
+    for (int i = 0; i < count; i++) {
+        _bufs.push_back(new char[size]);
+    }
+    _buf_size = size;
+}
+
+void rawmedia::decoded::fill_bufs(char** frames, int frame_size)
+{
+    for (uint i = 0; i < _bufs.size(); i++) {
+        memcpy(_bufs[i] + _data_size, frames[i], frame_size);
+    }
+    _data_size += frame_size;
+}
+
+void rawmedia::decoded::grow_bufs(int grow)
+{
+    for (uint i = 0; i < _bufs.size(); i++) {
+        char* buf = new char[_buf_size + grow];
+        memcpy(buf, _bufs[i], _data_size);
+        delete[] _bufs[i];
+        _bufs[i] = buf;
+    }
+    _buf_size += grow;
+}
+
+shared_ptr<rawmedia::decoded> rawmedia::extractor::extract(char* item, int item_size)
+{
+    auto _raw->= make_shared<rawmedia::decoded>();
+
+    _format = avformat_alloc_context();
+    if (_format == 0) {
+        throw runtime_error("Could not get context for decoding");
+    }
+    uchar* item_copy = (uchar*) av_malloc(item_size);
+    if (item_copy == 0) {
+        throw runtime_error("Could not allocate memory");
+    }
+
+    memcpy(item_copy, item, item_size);
+    _format->pb = avio_alloc_context(item_copy, item_size, 0, 0, 0, 0, 0);
+
+    if (avformat_open_input(&_format , "", 0, 0) < 0) {
+        throw runtime_error("Could not open input for decoding");
+    }
+
+    if (avformat_find_stream_info(_format, 0) < 0) {
+        throw runtime_error("Could not find media information");
+    }
+
+    _codec = _format->streams[0]->codec;
+    int stream = av_find_best_stream(_format, _avmedia_type, -1, -1, 0, 0);
+
+    if (stream < 0) {
+        throw runtime_error("Could not find media stream in input");
+    }
+
+    if (avcodec_open2(_codec, avcodec_find_decoder(_codec->codec_id), 0) < 0) {
+        throw runtime_error("Could not open decoder");
+    }
+
+    if (_raw->size() == 0) {
+        _raw->add_bufs(_codec->channels, item_size);
+    } else {
+        _raw->reset();
+    }
+
+    _raw->set_sample_size(av_get_bytes_per_sample(_codec->sample_fmt));
+
+    assert(_raw->get_sample_size() >= 0);
+
+    AVPacket packet;
+    while (av_read_frame(_format, &packet) >= 0) {
+        decode_frame(&packet, _raw, stream, item_size);
+    }
+
+    avcodec_close(_codec);
+    av_free(_format->pb->buffer);
+    av_free(_format->pb);
+    avformat_close_input(&_format);
+    return _raw;
+}
+
+void rawmedia::extractor::decode_frame(AVPacket* packet,
+                                       shared_ptr<rawmedia::decoded> raw,
+                                       int stream,
+                                       int item_size)
+{
+    int frame_done;
+    if (packet->stream_index == stream) {
+        AVFrame* frame = av_frame_alloc();
+        int result = 0;
+        if (_mediaType == AVMEDIA_TYPE_AUDIO) {
+            result = avcodec_decode_audio4(_codec, frame, &frame_done, packet);
+        } else {
+            throw runtime_error("Unsupported media");
+        }
+
+        if (result < 0) {
+            throw runtime_error("Could not decode media stream");
+        }
+
+        if (frame_done == true) {
+            int frame_size = frame->nb_samples * raw->get_sample_size();
+            if (raw->get_buf_size() < raw->get_data_size() + frame_size) {
+                raw->grow_bufs(item_size);
+            }
+            raw->fill_bufs((char**) frame->data, frame_size);
+        }
+        av_frame_free(&frame);
+    }
+    av_free_packet(packet);
+}
---
+++ loader/src/etl_rawmedia.hpp
@@ -0,0 +1,125 @@
+#pragma once
+
+#include "etl_interface.hpp"
+#include "params.hpp"
+#include <libavcodec/avcodec.h>
+#include <libavutil/common.h>
+
+
+namespace nervana {
+    namespace rawmedia {
+        class config;
+        class params;
+        class decoded;
+
+        class param_factory; // goes from config -> params
+
+        class extractor;
+        class transformer;
+        class loader;
+    }
+
+    class rawmedia::params : public nervana::params {
+    public:
+        params() {}
+        void dump(std::ostream & = std::cout) {}
+    };
+
+    class rawmedia::param_factory {
+    public:
+        param_factory(std::shared_ptr<rawmedia::config>);
+        ~param_factory() {}
+
+        std::shared_ptr<rawmedia::params> make_params(std::shared_ptr<const decoded>,
+                                                      std::default_random_engine&);
+    private:
+        std::shared_ptr<rawmedia::config> _icp;
+    };
+
+    class rawmedia::config : public json_config_parser {
+    public:
+
+        string media_type;
+        AVMediaType av_media_type = AVMEDIA_TYPE_UNKOWN;
+
+        config(std::string argString)
+        {
+            auto js = nlohmann::json::parse(argString);
+
+            parse_req(media_type, "media_type", js);
+
+            auto avt = _avtype_map.find(media_type);
+            if (avt == _avtype_map.end()) {
+                throw runtime_error("Unsupported media");
+            } else {
+                av_media_type = avt->second;
+            }
+
+            validate();
+        }
+
+    private:
+        bool validate() {}
+
+        std::unordered_map<std::string, AVMediaType> _avtype_map = {{"audio", AVMEDIA_TYPE_AUDIO},
+                                                                    {"video", AVMEDIA_TYPE_AUDIO}};
+
+    };
+
+    class rawmedia::decoded : public decoded_media {
+    public:
+        decoded(MediaType m) : _media_type(m) {}
+
+        virtual ~decoded() override
+        {
+            for (uint i = 0; i < _bufs.size(); i++) {
+                delete[] _bufs[i];
+            }
+        }
+
+        virtual MediaType get_type() override { return _media_type; }
+
+        inline void reset() { _data_size = 0; }
+        inline void set_sample_size(int sample_size) { _sample_size = sample_size; }
+        inline char* get_buf(int idx) { return _bufs[idx]; }
+        inline int get_size() { return _bufs.size(); }
+        inline int get_buf_size() { return _buf_size; }
+        inline int get_data_size() { return _data_size; }
+        inline int get_sample_size() { return _sample_size; }
+
+        void add_bufs(int count, int size);
+        void fill_bufs(char** frames, int frame_size);
+        void grow_bufs(int grow);
+        void copy_data(char* buf, int buf_size);
+
+    private:
+        int _buf_size    = 0;
+        int _data_size   = 0;
+        int _sample_size = 0;
+        std::vector<char*> _bufs;
+        MediaType _media_type;
+    };
+
+
+    class rawmedia::extractor : public interface::extractor<rawmedia::decoded> {
+    public:
+        extractor(std::shared_ptr<const rawmedia::config> cfg)
+        {
+            _av_media_type = cfg->av_media_type;
+        }
+
+        ~extractor() {}
+        virtual std::shared_ptr<rawmedia::decoded> extract(char*, int) override;
+
+    private:
+        void decode_frame(AVPacket* packet,
+                          std::shared_ptr<rawmedia::decoded> raw,
+                          int stream,
+                          int itemSize);
+
+        AVMediaType      _av_media_type;
+        AVFormatContext* _format;
+        AVCodecContext*  _codec;
+    };
+
+}
