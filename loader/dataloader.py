43e99e0cac3167eedad69848b149d52fc9e0a0d6
Zach Dwiel <zach@nervanasys.com>
2016-07-08 13:10:35 -0700

    rename dloader.py -> dataloader.py; change DataLoader to follow one epoch per call to __iter__ pattern

--- loader/dataloader.py
+++ loader/dataloader.py
@@ -16,6 +16,7 @@ import ctypes as ct
 import os
 import atexit
 import json
+import math
 
 
 class DataLoader(object):
@@ -43,14 +44,20 @@ class DataLoader(object):
     """
 
     def __init__(self, config, backend):
-        self.buffer_id = 0
+        self._buffer_id = 0
+        self._item_index = 0
 
         self._load_library()
+
+        self.minibatch_size = config['minibatch_size']
 
         # Launch background threads
         self.loader = self._start(json.dumps(config), backend)
 
-        atexit.register(self._stop, self)
+        atexit.register(self._stop)
+
+        # compute the number of minibatches which will be in the first epoch
+        self._compute_nbatches()
 
     def _load_library(self):
         path = os.path.dirname(os.path.realpath(__file__))
@@ -110,12 +117,12 @@ class DataLoader(object):
 
         return itemCount
 
-    def _next(self, buffer_id):
+    def _next(self, _buffer_id):
         """
         C api wrapper with exception handling
         """
         tup = self.loaderlib.next(
-            self.loader, ct.c_int(buffer_id)
+            self.loader, ct.c_int(_buffer_id)
         )
 
         if tup is None:
@@ -131,7 +138,7 @@ class DataLoader(object):
             self._raise_loader_error()
 
     @property
-    def itemCount(self):
+    def item_count(self):
         """
         Number of items in the dataset.
         """
@@ -141,23 +148,87 @@ class DataLoader(object):
         """
         Restart data from index 0.
         """
-        self.buffer_id = 0
+        self._buffer_id = 0
+        self._item_index = 0
+
         self._reset()
 
     def next(self):
         """
         return one minibatch in a (data, targets) tuple
         """
-        (data, targets) = self._next(self.buffer_id)
+        (data, targets) = self._next(self._buffer_id)
 
-        # Toggle buffer_id between 0 and 1
-        self.buffer_id = 1 - self.buffer_id
+        # Toggle _buffer_id between 0 and 1
+        self._buffer_id = 1 - self._buffer_id
 
         return (data, targets)
 
-    def __iter__(self):
+    def unending_iter(self):
         """
         never ending iterator over dataset.
         """
         while True:
             yield self.next()
+
+    def _compute_nbatches(self):
+        """
+        compute the number of batches for the next epoch
+        """
+
+        self._nbatches = math.ceil(
+            (self.item_count - self._item_index) / float(self.minibatch_size)
+        )
+
+    @property
+    def nbatches(self):
+        """
+        Returns the number of minibatches in this dataset in this epoch.
+
+        Sometimes the number of minibatches per epoch changes since items from
+        the next epoch will be used to fill the last minibatch of an epoch if
+        it doesn't line up with the end of the epoch.
+        """
+        return self._nbatches
+
+    def __iter__(self):
+        """
+        Iterate over a single epoch.
+
+        If the last minibatch is not full, use data from the next epoch to fill
+        it.  The next epoch will not repeat the data used to fill the last
+        minibatch.
+        """
+
+        self._compute_nbatches()
+
+        for _ in range(self._nbatches):
+            yield self.next()
+
+            # keep track of where we are in the dataset so we know which epoch
+            # we are on
+            self._item_index += self.minibatch_size
+            if self._item_index > self.item_count:
+                self._item_index -= self.item_count
+
+    # UNUSED
+    # these are reference for how we could switch to explicit epoch handling
+    # rather than the implicit handling we have right now.
+
+    def start_item_index(self, epoch_index):
+        """ The starting item offset for epoch # `epoch_index`
+
+        assumes 0 indexed epochs """
+        return (self.item_count * epoch_index) % self.minibatch_size
+
+    def nbatches_at_epoch(self, epoch_index):
+        """ returns the number of minibatches which will be in epoch #
+        `epoch_index` """
+        return math.ceil(
+            (self.item_count - self.start_item_index(epoch_index)) / float(self.minibatch_size)
+        )
+
+    def minibatch_index(self, epoch_index):
+        """ returns the minibatch_index that `epoch_index` starts with """
+        return (self.item_count * epoch_index) // self.minibatch_size
+
