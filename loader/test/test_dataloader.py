153e44f70af54aa35456dd046231d8f098dd8c2b
Zach Dwiel <zach@nervanasys.com>
2016-07-20 16:34:02 -0700

    first pass at integration test against python DataLoader

---
+++ loader/test/test_dataloader.py
@@ -0,0 +1,137 @@
+import tempfile
+
+import numpy as np
+from PIL import Image as PILImage
+import random
+import struct
+import pytest
+
+from neon.backends import gen_backend
+from neon.models import Model
+from loader.dataloader import DataLoader
+
+
+def random_image(filename):
+    """
+    generate a small random image
+    """
+    a = np.random.random((2, 2, 3)).astype('uint8')
+    img = PILImage.fromarray(a)
+    img.save(filename)
+
+
+def invalid_image(filename):
+    """
+    write an empty file to filename to trigger invalid image file exceptions
+    """
+    with open(filename, 'w') as f:
+        pass
+
+
+def random_target(filename):
+    target = int(random.random() * 1024)
+
+    with open(filename, 'wb') as f:
+        f.write(struct.pack('i', target))
+
+    return filename
+
+
+def random_manifest(num_lines, invalid_image_index=None):
+    manifest = tempfile.NamedTemporaryFile(mode='w')
+
+    # generate a manifest of filenames with an invalid image on the 3rd line
+    for i in range(num_lines):
+        img_filename = tempfile.mkstemp(suffix='.jpg')[1]
+        if i == invalid_image_index:
+            invalid_image(img_filename)
+        else:
+            random_image(img_filename)
+
+        target_filename = tempfile.mkstemp(suffix='.jpg')[1]
+        random_target(target_filename)
+
+        manifest.write("{},{}\n".format(img_filename, target_filename))
+    manifest.flush()
+
+    return manifest
+
+
+def generic_config(manifest):
+    return {
+        'manifest_filename': manifest.name,
+        'minibatch_size': 2,
+        'data_config': {
+            'type': 'image',
+            'config': {
+                'height': 2,
+                'width': 2,
+            },
+        },
+        'target_config': {
+            'type': 'image',
+            'config': {
+                'height': 2,
+                'width': 2,
+            },
+        },
+        'media': 'image_label',
+    }
+
+
+def test_loader():
+    # NOTE: manifest needs to stay in scope until DataLoader has read it.
+    manifest = random_manifest(10)
+    config = generic_config(manifest)
+
+    dl = DataLoader(config, gen_backend(backend='cpu'))
+
+    assert len(list(iter(dl))) == 5
+
+
+def test_loader_repeat_iter():
+    # NOTE: manifest needs to stay in scope until DataLoader has read it.
+    manifest = random_manifest(10)
+    config = generic_config(manifest)
+
+    dl = DataLoader(config, gen_backend(backend='cpu'))
+
+    assert len(list(iter(dl))) == 5
+    assert len(list(iter(dl))) == 5
+
+
+def test_loader_exception_next():
+    # NOTE: manifest needs to stay in scope until DataLoader has read it.
+    manifest = random_manifest(10, 2)
+    config = generic_config(manifest)
+
+    dl = DataLoader(config, gen_backend(backend='cpu'))
+    dl.next()
+    with pytest.raises(Exception):
+        dl.next()
+
+
+def test_loader_exception_iter():
+    # NOTE: manifest needs to stay in scope until DataLoader has read it.
+    manifest = random_manifest(10, 2)
+    config = generic_config(manifest)
+
+    dl = DataLoader(config, gen_backend(backend='cpu'))
+
+    assert len(list(iter(dl))) == 4
+
+
+def test_loader_reset():
+    # NOTE: manifest needs to stay in scope until DataLoader has read it.
+    manifest = random_manifest(10)
+    config = generic_config(manifest)
+
+    dl = DataLoader(config, gen_backend(backend='cpu'))
+
+    assert len(list(iter(dl))) == 5
+    dl.reset()
+    assert len(list(iter(dl))) == 5
+
+
+if __name__ == '__main__':
+    pytest.main('-s')
