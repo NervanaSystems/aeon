52de1803d5e27b91b1ef5135f8226d050ceaef36
Alex Park <alex.park@gmail.com>
2016-08-08 12:37:01 -0700

    Some doc build changes (virtualenv prereqs, exclude json.hpp)

--- doc/install_prereqs.sh
+++ doc/install_prereqs.sh
@@ -5,4 +5,10 @@ sudo apt-get install doxygen
 # We have to use the master version of breathe to avoid
 # https://github.com/michaeljones/breathe/issues/261
 # I've pinned the commit to what worked for me
-sudo pip install Sphinx git+https://github.com/michaeljones/breathe.git@d681785bb728eb02261263f26736bf6b2a929da5
+
+if [[ "$VIRTUAL_ENV" != "" ]]
+then
+    pip install Sphinx git+https://github.com/michaeljones/breathe.git@d681785bb728eb02261263f26736bf6b2a929da5
+else
+    sudo pip install Sphinx git+https://github.com/michaeljones/breathe.git@d681785bb728eb02261263f26736bf6b2a929da5
+fi
