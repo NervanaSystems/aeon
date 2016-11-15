77b85b70fb18e7fa9b72ef83e802e99ef0b67f6c
Alex Park <alex.park@gmail.com>
2016-11-15 15:03:02 -0800

    Update doc version to match release

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,7 +73,7 @@ copyright = u'2016, Nervana Systems Inc'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.0'
+version = '0.2.5'
 # The full version, including alpha/beta/rc tags.
 release = version
 
