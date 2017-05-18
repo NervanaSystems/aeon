255ad661264ace5987be1396e9322703c136cb58
tsocha <tomasz.socha@intel.com>
2017-05-18 16:20:02 +0200

    Added clang-format to cmake (#46)

---
+++ clang-format.cmake
@@ -0,0 +1,14 @@
+file(GLOB_RECURSE STYLE_SRC
+	${PROJECT_SOURCE_DIR}/src/*.cpp
+	${PROJECT_SOURCE_DIR}/test/*.cpp
+	${PROJECT_SOURCE_DIR}/src/*.hpp
+	${PROJECT_SOURCE_DIR}/test/*.hpp
+	${PROJECT_SOURCE_DIR}/src/*.h
+	${PROJECT_SOURCE_DIR}/test/*.h)
+
+list(REMOVE_ITEM STYLE_SRC "${PROJECT_SOURCE_DIR}/src/json.hpp")
+
+add_custom_target(
+	style
+	COMMAND /usr/bin/clang-format -style=file
+	-i ${STYLE_SRC})
