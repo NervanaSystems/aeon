ce57d968bc1a786c610ba6c9e45b7c8c7a4f9525
Sylwester Fraczek <sylwester.fraczek@intel.com>
2018-03-05 11:29:44 +0100

    updated copyright notices (#257)
    * updated copyright notices
    
    dos2unix run
    justify apache LICENSE-2.0 url
    Added newline after 'Copyright Intel Corporation'
    * separate comment # from star * with space ' '

--- examples/augmentation_assistant/main.cpp
+++ examples/augmentation_assistant/main.cpp
@@ -1,17 +1,18 @@
-/*
- Copyright 2017 Intel(R) Nervana(TM)
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+/*******************************************************************************
+* Copyright 2017-2018 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
 
 #include <iostream>
 
@@ -131,7 +132,7 @@ public:
                 out << "    <div class=\"col-sm-3\">\n";
                 out << "      <input type=\"number\" class=\"form-control\" id=\"" << x->name() << "\">\n";
                 out << "    </div>\n";
-            } 
+            }
             else if (x->type() == "std::string")
             {
                 out << "    <label for=\"" << x->name() << "\" class=\"col-sm-2 control-label\">" << x->name() << "</label>\n";
