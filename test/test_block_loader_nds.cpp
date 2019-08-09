4b3bec38fc14598087a7598b32dcb97f29f6a19c
Sylwester Fraczek <sylwester.fraczek@intel.com>
2019-08-09 15:44:40 +0200

    disable nds in tests

--- test/test_block_loader_nds.cpp
+++ test/test_block_loader_nds.cpp
@@ -50,179 +50,179 @@ TEST(DISABLED_curl, test)
     }
 }
 
-TEST(block_loader_nds, curl_stream)
-{
-    size_t block_size          = 16;
-    size_t elements_per_record = 2;
+// TEST(block_loader_nds, curl_stream)
+// {
+//     size_t block_size          = 16;
+//     size_t elements_per_record = 2;
 
-    shared_ptr<manifest_nds> client(manifest_nds_builder()
-                                        .base_url("http://127.0.0.1:5000")
-                                        .token("token")
-                                        .collection_id(1)
-                                        .block_size(block_size)
-                                        .elements_per_record(elements_per_record)
-                                        .make_shared());
+//     shared_ptr<manifest_nds> client(manifest_nds_builder()
+//                                         .base_url("http://127.0.0.1:5000")
+//                                         .token("token")
+//                                         .collection_id(1)
+//                                         .block_size(block_size)
+//                                         .elements_per_record(elements_per_record)
+//                                         .make_shared());
 
-    stringstream stream;
-    client->m_network_client.get("http://127.0.0.1:5000/test_pattern/", stream);
+//     stringstream stream;
+//     client->m_network_client.get("http://127.0.0.1:5000/test_pattern/", stream);
 
-    stringstream expected;
-    for (int i = 0; i < 1024; ++i)
-    {
-        expected << "0123456789abcdef";
-    }
-    ASSERT_EQ(stream.str(), expected.str());
-}
+//     stringstream expected;
+//     for (int i = 0; i < 1024; ++i)
+//     {
+//         expected << "0123456789abcdef";
+//     }
+//     ASSERT_EQ(stream.str(), expected.str());
+// }
 
-TEST(block_loader_nds, curl_stream_filename)
-{
-    size_t block_size          = 16;
-    size_t elements_per_record = 2;
+// TEST(block_loader_nds, curl_stream_filename)
+// {
+//     size_t block_size          = 16;
+//     size_t elements_per_record = 2;
 
-    nlohmann::json json;
-    json["url"]                     = "http://127.0.0.1:5000";
-    json["params"]["token"]         = "token";
-    json["params"]["collection_id"] = 1;
-    json["params"]["tag"]           = "train";
+//     nlohmann::json json;
+//     json["url"]                     = "http://127.0.0.1:5000";
+//     json["params"]["token"]         = "token";
+//     json["params"]["collection_id"] = 1;
+//     json["params"]["tag"]           = "train";
 
-    std::string   tmp_filename = nervana::file_util::tmp_filename();
-    std::ofstream ofs(tmp_filename);
+//     std::string   tmp_filename = nervana::file_util::tmp_filename();
+//     std::ofstream ofs(tmp_filename);
 
-    json >> ofs;
-    ofs.close();
+//     json >> ofs;
+//     ofs.close();
 
-    manifest_nds client = manifest_nds_builder()
-                              .filename(tmp_filename)
-                              .block_size(block_size)
-                              .elements_per_record(elements_per_record)
-                              .create();
+//     manifest_nds client = manifest_nds_builder()
+//                               .filename(tmp_filename)
+//                               .block_size(block_size)
+//                               .elements_per_record(elements_per_record)
+//                               .create();
 
-    stringstream stream;
-    client.m_network_client.get("http://127.0.0.1:5000/test_pattern/", stream);
+//     stringstream stream;
+//     client.m_network_client.get("http://127.0.0.1:5000/test_pattern/", stream);
 
-    stringstream expected;
-    for (int i = 0; i < 1024; ++i)
-    {
-        expected << "0123456789abcdef";
-    }
-    ASSERT_EQ(stream.str(), expected.str());
-}
+//     stringstream expected;
+//     for (int i = 0; i < 1024; ++i)
+//     {
+//         expected << "0123456789abcdef";
+//     }
+//     ASSERT_EQ(stream.str(), expected.str());
+// }
 
-TEST(block_loader_nds, curl_stream_error)
-{
-    size_t block_size          = 16;
-    size_t elements_per_record = 2;
+// TEST(block_loader_nds, curl_stream_error)
+// {
+//     size_t block_size          = 16;
+//     size_t elements_per_record = 2;
 
-    shared_ptr<manifest_nds> client(manifest_nds_builder()
-                                        .base_url("http://127.0.0.1:5000")
-                                        .token("token")
-                                        .collection_id(1)
-                                        .block_size(block_size)
-                                        .elements_per_record(elements_per_record)
-                                        .make_shared());
+//     shared_ptr<manifest_nds> client(manifest_nds_builder()
+//                                         .base_url("http://127.0.0.1:5000")
+//                                         .token("token")
+//                                         .collection_id(1)
+//                                         .block_size(block_size)
+//                                         .elements_per_record(elements_per_record)
+//                                         .make_shared());
 
-    stringstream stream;
-    EXPECT_THROW(client->m_network_client.get("http://127.0.0.1:5000/error", stream),
-                 std::runtime_error);
-}
+//     stringstream stream;
+//     EXPECT_THROW(client->m_network_client.get("http://127.0.0.1:5000/error", stream),
+//                  std::runtime_error);
+// }
 
-TEST(block_loader_nds, record_count)
-{
-    size_t block_size          = 16;
-    size_t elements_per_record = 2;
+// TEST(block_loader_nds, record_count)
+// {
+//     size_t block_size          = 16;
+//     size_t elements_per_record = 2;
 
-    shared_ptr<manifest_nds> client(manifest_nds_builder()
-                                        .base_url("http://127.0.0.1:5000")
-                                        .token("token")
-                                        .collection_id(1)
-                                        .block_size(block_size)
-                                        .elements_per_record(elements_per_record)
-                                        .make_shared());
+//     shared_ptr<manifest_nds> client(manifest_nds_builder()
+//                                         .base_url("http://127.0.0.1:5000")
+//                                         .token("token")
+//                                         .collection_id(1)
+//                                         .block_size(block_size)
+//                                         .elements_per_record(elements_per_record)
+//                                         .make_shared());
 
-    // 200 and 5 are hard coded in the mock nds server
-    ASSERT_EQ(client->record_count(), 200);
-    ASSERT_EQ(client->block_count(), 5);
-}
+//     // 200 and 5 are hard coded in the mock nds server
+//     ASSERT_EQ(client->record_count(), 200);
+//     ASSERT_EQ(client->block_count(), 5);
+// }
 
-TEST(block_loader_nds, cpio)
-{
-    size_t block_size          = 16;
-    size_t elements_per_record = 2;
-    size_t block_count         = 3;
+// TEST(block_loader_nds, cpio)
+// {
+//     size_t block_size          = 16;
+//     size_t elements_per_record = 2;
+//     size_t block_count         = 3;
 
-    shared_ptr<manifest_nds> client(manifest_nds_builder()
-                                        .base_url("http://127.0.0.1:5000")
-                                        .token("token")
-                                        .collection_id(1)
-                                        .block_size(block_size)
-                                        .elements_per_record(elements_per_record)
-                                        .make_shared());
+//     shared_ptr<manifest_nds> client(manifest_nds_builder()
+//                                         .base_url("http://127.0.0.1:5000")
+//                                         .token("token")
+//                                         .collection_id(1)
+//                                         .block_size(block_size)
+//                                         .elements_per_record(elements_per_record)
+//                                         .make_shared());
 
-    size_t record_number = 0;
-    for (size_t block_number = 0; block_number < block_count; block_number++)
-    {
-        encoded_record_list* block = client->load_block(block_number);
-        ASSERT_EQ(block_size, block->size());
+//     size_t record_number = 0;
+//     for (size_t block_number = 0; block_number < block_count; block_number++)
+//     {
+//         encoded_record_list* block = client->load_block(block_number);
+//         ASSERT_EQ(block_size, block->size());
 
-        for (auto record : *block)
-        {
-            element_info info0(vector2string(record.element(0)));
-            element_info info1(vector2string(record.element(1)));
+//         for (auto record : *block)
+//         {
+//             element_info info0(vector2string(record.element(0)));
+//             element_info info1(vector2string(record.element(1)));
 
-            ASSERT_EQ(record_number, info0.record_number());
-            ASSERT_EQ(record_number, info1.record_number());
-            ASSERT_EQ(0, info0.element_number());
-            ASSERT_EQ(1, info1.element_number());
+//             ASSERT_EQ(record_number, info0.record_number());
+//             ASSERT_EQ(record_number, info1.record_number());
+//             ASSERT_EQ(0, info0.element_number());
+//             ASSERT_EQ(1, info1.element_number());
 
-            record_number++;
-        }
-    }
-}
+//             record_number++;
+//         }
+//     }
+// }
 
-TEST(block_loader_nds, cpio_filename)
-{
-    size_t block_size          = 16;
-    size_t elements_per_record = 2;
-    size_t block_count         = 3;
+// TEST(block_loader_nds, cpio_filename)
+// {
+//     size_t block_size          = 16;
+//     size_t elements_per_record = 2;
+//     size_t block_count         = 3;
 
-    nlohmann::json json;
-    json["url"]                     = "http://127.0.0.1:5000";
-    json["params"]["token"]         = "token";
-    json["params"]["collection_id"] = 1;
-    json["params"]["tag"]           = "train";
+//     nlohmann::json json;
+//     json["url"]                     = "http://127.0.0.1:5000";
+//     json["params"]["token"]         = "token";
+//     json["params"]["collection_id"] = 1;
+//     json["params"]["tag"]           = "train";
 
-    std::string   tmp_filename = nervana::file_util::tmp_filename();
-    std::ofstream ofs(tmp_filename);
+//     std::string   tmp_filename = nervana::file_util::tmp_filename();
+//     std::ofstream ofs(tmp_filename);
 
-    json >> ofs;
-    ofs.close();
+//     json >> ofs;
+//     ofs.close();
 
-    manifest_nds client = manifest_nds_builder()
-                              .filename(tmp_filename)
-                              .block_size(block_size)
-                              .elements_per_record(elements_per_record)
-                              .create();
+//     manifest_nds client = manifest_nds_builder()
+//                               .filename(tmp_filename)
+//                               .block_size(block_size)
+//                               .elements_per_record(elements_per_record)
+//                               .create();
 
-    size_t record_number = 0;
-    for (size_t block_number = 0; block_number < block_count; block_number++)
-    {
-        encoded_record_list* block = client.load_block(block_number);
-        ASSERT_EQ(block_size, block->size());
+//     size_t record_number = 0;
+//     for (size_t block_number = 0; block_number < block_count; block_number++)
+//     {
+//         encoded_record_list* block = client.load_block(block_number);
+//         ASSERT_EQ(block_size, block->size());
 
-        for (auto record : *block)
-        {
-            element_info info0(vector2string(record.element(0)));
-            element_info info1(vector2string(record.element(1)));
+//         for (auto record : *block)
+//         {
+//             element_info info0(vector2string(record.element(0)));
+//             element_info info1(vector2string(record.element(1)));
 
-            ASSERT_EQ(record_number, info0.record_number());
-            ASSERT_EQ(record_number, info1.record_number());
-            ASSERT_EQ(0, info0.element_number());
-            ASSERT_EQ(1, info1.element_number());
+//             ASSERT_EQ(record_number, info0.record_number());
+//             ASSERT_EQ(record_number, info1.record_number());
+//             ASSERT_EQ(0, info0.element_number());
+//             ASSERT_EQ(1, info1.element_number());
 
-            record_number++;
-        }
-    }
-}
+//             record_number++;
+//         }
+//     }
+// }
 
 // TEST(block_loader_nds, multiblock_sequential)
 // {
